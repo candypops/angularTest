9a14099d06ec0590a383b827d9311f402fc1261d
maria gabriela <mariagabriela236@gmail.com>
2022-07-11 23:00:22 -0400

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { LayoutModule } from './layout/layout.module';
 
+const routes: Routes = [
+  {
+      path: '',
+      component: DashboardComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
