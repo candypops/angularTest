2817eee97ebae93458af9fdb26f076731d31efcb
maria gabriela <mariagabriela236@gmail.com>
2022-07-11 23:46:46 -0400

    refactor ordenando archivos

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, SidenavComponent],
   imports: [
     CommonModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    RouterModule
+
   ]
 })
 export class LayoutModule { }
