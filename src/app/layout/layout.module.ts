79ab3227ee71c5680480f25e4b3938abab94de61
maria gabriela <mariagabriela236@gmail.com>
2022-07-11 23:25:57 -0400

    import sidenav

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
+    MatSidenavModule
   ]
 })
 export class LayoutModule { }
