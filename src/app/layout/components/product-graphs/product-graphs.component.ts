85cfafb55d2ab13ec25997d223709dab122b17ba
maria gabriela <mariagabriela236@gmail.com>
2022-07-13 00:26:36 -0400

    grafico funcional

--- src/app/layout/components/product-graphs/product-graphs.component.ts
+++ src/app/layout/components/product-graphs/product-graphs.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 import {
-ChartComponent,
-ApexAxisChartSeries,
-ApexChart,
-ApexXAxis,
-ApexTitleSubtitle
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
 } from "ng-apexcharts";
 
 export type ChartOptions = {
-series: ApexAxisChartSeries;
-chart: ApexChart;
-xaxis: ApexXAxis;
-title: ApexTitleSubtitle;
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
 };
 
 @Component({
@@ -19,33 +19,51 @@ title: ApexTitleSubtitle;
   templateUrl: './product-graphs.component.html',
   styleUrls: ['./product-graphs.component.scss']
 })
-export class ProductGraphsComponent implements OnInit {
+export class ProductGraphsComponent implements OnInit, OnChanges {
 
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
+  @Input() dataSource = [];
 
   constructor() {
     this.chartOptions = {
       series: [
         {
           name: "Stock",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+          data: []
         }
       ],
       chart: {
         height: 350,
-        type: "bar"
+        type: "bar",
+        zoom: {
+          enabled: true
+        }
       },
       title: {
         text: "Quiebres de stock"
       },
       xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+        type: "category",
+        categories: []
       }
     };
-   }
+  }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges() {
+    const series = [];
+    const categories = [];
+    this.dataSource.forEach(d => {
+      series.push(d?.count)
+      categories.push(d?._id)
+    })
+
+    this.chartOptions.series = [{ data: series }];
+    this.chartOptions = {...this.chartOptions, xaxis: {type: "category", categories: categories, tickPlacement: 'on'}}
+
+
+  }
 }
